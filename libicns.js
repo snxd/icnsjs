9d507dd292d174263ed8c76c6a174992cd1e351e
Jason Lines <jlines@solidstatenetworks.com>
2015-03-11 16:47:47 -0700

    add toc parsing

--- libicns.js
+++ libicns.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 var _ = require('lodash');
 
-exports.readItem = function(filePath, offset, length, next) {
+exports.readItem = function(filePath, offset, next) {
 	fs.open(filePath, 'r', function(status, fd) {
 	    if (status) {
 	        //console.log(status.message);
 	        return next(status.message);
 	    }
-	    var buffer = new Buffer(length);
-	    fs.read(fd, buffer, 0, length, offset, function(err, num) {
+	    var buffer = new Buffer(8);
+	    fs.read(fd, buffer, 0, 8, offset, function(err, num) {
 	        if(err) {
 	        	return next(err);
 	        }
@@ -19,43 +19,40 @@ exports.readItem = function(filePath, offset, length, next) {
 };
 
 exports.isValidIcns = function(filePath, next) {
-	this.readItem(filePath, 0, 8, function(err, label, size) {
-
+	this.readItem(filePath, 0, function(err, label, size) {
+		next(null, label === 'icns');
 	});
 };
 
-exports.fileSize = function(filePath, next) {
-	fs.open(filePath, 'r', function(status, fd) {
-	    if (status) {
-	        //console.log(status.message);
-	        return next(status.message);
-	    }
-	    console.log('test');
-	    var buffer = new Buffer(4);
-	    fs.read(fd, buffer, 0, 4, 4, function(err, num) {
-	        if(err) {
-	        	return next(err);
-	        }
+exports.loadToc = function(filePath, next) {
+	exports.readItem(filePath, 8, function(err, label, tocSize) {
+		if(label.trim() !== 'TOC') {
+			console.log(label);
+			return next(new Error('Unable to find a valid TOC in file'));
+		}
 
-	        return next(null, buffer.readUInt32BE(0));
-	    });
-	});
-};
+		var toc = [];
 
-exports.readTOC = function(filePath, offset, next) {
-	fs.open(filePath, 'r', function(status, fd) {
-	    if (status) {
-	        //console.log(status.message);
-	        return next(status.message);
-	    }
-	    var buffer = new Buffer(4);
-	    fs.read(fd, buffer, 0, 4, 4, function(err, num) {
-	        if(err) {
-	        	return next(err);
-	        }
+		fs.open(filePath, 'r', function(status, fd) {
+		    if (status) {
+		        //console.log(status.message);
+		        return next(status.message);
+		    }
+		    var buffer = new Buffer(tocSize);
+		    fs.read(fd, buffer, 0, tocSize, 8, function(err, num) {
+		        if(err) {
+		        	return next(err);
+		        }
 
-	        return next(null, buffer.readUInt32BE(0));
-	    });
+		        for(var i=8; i<tocSize; i+=8) {
+		        	label = buffer.toString('utf-8', i, i+4);
+		        	size = buffer.readUInt32BE(i+4);
+		        	toc.push({format:label, size:size});
+		        }
+
+		        return next(null, toc);
+		    });
+		});
 	});
 };
 
@@ -63,8 +60,8 @@ exports.writePart = function(filePath, outputFile, offset, length) {
 	fs.createReadStream(filePath, {start:offset, end:offset+length}).pipe(fs.createWriteStream(outputFile));
 }
 
-exports.printItem = function(filePath, offset, length) {
-	this.readItem(filePath, offset, length, function(err, label, size) {
+exports.printItem = function(filePath, offset) {
+	this.readItem(filePath, offset, function(err, label, size) {
 		if(err) {
 			console.log('err ', err);
 		}
@@ -74,8 +71,16 @@ exports.printItem = function(filePath, offset, length) {
 
 exports.printDetails = function(filePath) {
 	_.map([0,1,2,3,4,5,6,7,8,9,10,11,12], function(n) {
-		exports.printItem(filePath, n*8, 8);
+		exports.printItem(filePath, n*8);
 	});
 
-	exports.printItem(filePath, 37836, 8);
+	exports.printItem(filePath, 37836);
+}
+
+exports.doSimple = function() {
+	exports.printDetails('./app.icns');
+	exports.loadToc('./app.icns', function(err, toc) {
+		console.log(toc);
+	});
+	//exports.writePart('./app.icns', './out.png', 37844, 25657);
 }
