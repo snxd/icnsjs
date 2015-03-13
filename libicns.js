d739fc541f83f5f5d95c056f417d48fe43713131
Jason Lines <jlines@solidstatenetworks.com>
2015-03-12 21:08:50 -0700

    refactor to sync file io

--- libicns.js
+++ libicns.js
@@ -1,80 +1,59 @@
 var fs = require('fs');
 var _ = require('lodash');
 
-exports.readItem = function(filePath, offset, next) {
-	fs.open(filePath, 'r', function(status, fd) {
-	    if (status) {
-	        //console.log(status.message);
-	        return next(status.message);
-	    }
-	    var buffer = new Buffer(8);
-	    fs.read(fd, buffer, 0, 8, offset, function(err, num) {
-	        if(err) {
-	        	return next(err);
-	        }
+exports.readItem = function(filePath, offset) {
+	var fd = fs.openSync(filePath, 'r');
+    var buffer = new Buffer(8);
+    fs.readSync(fd, buffer, 0, 8, offset);
+    fs.closeSync(fd);
 
-	        return next(null, buffer.toString('utf-8', 0, 4), buffer.readUInt32BE(4));
-	    });
-	});
+    return {label:buffer.toString('utf-8', 0, 4), size:buffer.readUInt32BE(4)};
 };
 
-exports.isValidIcns = function(filePath, next) {
-	this.readItem(filePath, 0, function(err, label, size) {
-		next(null, label === 'icns');
-	});
+exports.isValidIcns = function(filePath) {
+	var header = this.readItem(filePath, 0);
+
+	return header.label.trim() === 'icns';
 };
 
-exports.loadToc = function(filePath, next) {
-	exports.readItem(filePath, 8, function(err, label, tocSize) {
-		if(label.trim() !== 'TOC') {
-			console.log(label);
-			return next(new Error('Unable to find a valid TOC in file'));
-		}
+exports.loadToc = function(filePath) {
+	var tocHeader = this.readItem(filePath, 8);
+	if(tocHeader.label.trim() !== 'TOC') {
+		throw new Error('Unable to find a valid TOC in file');
+	}
 
-		var toc = [];
+	var toc = [];
 
-		fs.open(filePath, 'r', function(err, fd) {
-		    if (err) {
-		        //console.log(status.message);
-		        return next(err.message);
-		    }
-		    var buffer = new Buffer(tocSize);
-		    fs.read(fd, buffer, 0, tocSize, 8, function(err, num) {
-		        if(err) {
-		        	return next(err);
-		        }
+	var fd = fs.openSync(filePath, 'r');
+	    
+    var buffer = new Buffer(tocHeader.size);
+    var num = fs.readSync(fd, buffer, 0, tocHeader.size, 8);
+	var offset = 8;
 
-		        var offset = 8;
+    for(var i=0; i<tocHeader.size; i+=8) {
+    	var label = buffer.toString('utf-8', i, i+4);
+    	var size = buffer.readUInt32BE(i+4);
+    	toc.push({format:label, size:size, offset:offset});
+    	offset += size;
+    }
 
-		        for(var i=0; i<tocSize; i+=8) {
-		        	label = buffer.toString('utf-8', i, i+4);
-		        	size = buffer.readUInt32BE(i+4);
-		        	toc.push({format:label, size:size, offset:offset});
-		        	offset += size;
-		        }
-
-		        return next(null, toc);
-		    });
-		});
-	});
+    return toc;
 };
 
-exports.getPngReadStream = function(filePath, fetchIconType, next) {
-	exports.loadToc(filePath, function(err, toc) {
-		entry = _.where(toc, {format:fetchIconType});
-		if(entry.length < 1) {
-			return next('Icon type not found:' + fetchIconType);
-		}
+exports.getIconReadStream = function(filePath, fetchIconType) {
+	var toc = this.loadToc(filePath);
+	var entry = _.where(toc, {format:fetchIconType});
+	if(entry.length < 1) {
+		throw new Error('Icon type not found:' + fetchIconType);
+	}
 
-		//offset for icon header
-		entry[0].offset += 8;
-		entry[0].size -= 8;
+	//offset for icon header
+	entry[0].offset += 8;
+	entry[0].size -= 8;
 
-		//var stream = fs.createReadStream(filePath, {start:entry.offset, end:entry.offset+entry.size})		
-		var iconRange = {start:entry[0].offset, end:entry[0].offset+entry[0].size};
-		console.log(iconRange);
-		next(null, fs.createReadStream(filePath, iconRange));
-	});
+	//var stream = fs.createReadStream(filePath, {start:entry.offset, end:entry.offset+entry.size})		
+	var iconRange = {start:entry[0].offset, end:entry[0].offset+entry[0].size};
+	return fs.createReadStream(filePath, iconRange);
 }
 
 exports.printItem = function(filePath, offset) {
@@ -87,18 +66,24 @@ exports.printItem = function(filePath, offset) {
 }
 
 exports.doSimple = function() {
+	var f = './app.icns';
+	var self = this;
 	//exports.printDetails('./app.icns');
-	exports.loadToc('./app.icns', function(err, toc) {
-		console.log(toc);
-	});
+	console.log('valid Icns:', this.isValidIcns(f));
+
+	var toc = this.loadToc(f);
+	console.log(toc);
+
+	/*_.map(toc, function(item) {
+		var fmt = item.format;
+		if(fmt.trim() === 'TOC') return;
+
+		self.getIconReadStream(f, fmt)
+			.pipe(fs.createWriteStream(fmt + '-out.png'));
+	});*/
 	//exports.writePart('./app.icns', './out.png', 37844, 25657);
 	var fmt = 'ic08';
-	exports.getPngReadStream('./app.icns', fmt, function(err, stream) {
-		if(err) {
-			return console.log('err:', err);
-		}
-		stream.pipe(fs.createWriteStream(fmt + '-out.png'));
-	});
+	this.getIconReadStream(f, fmt).pipe(fs.createWriteStream(fmt + '-out.png'));
 }
 
 exports.doSimple();
